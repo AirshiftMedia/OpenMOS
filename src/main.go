618dc076a73cc7e27f83610569467bf4863856c4
Timo Ruohomaki <timo.ruohomaki@forumvirium.fi>
2023-07-02 11:44:20 +0300

    added Sentry init

--- src/main.go
+++ src/main.go
@@ -4,9 +4,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 )
 
+buildVersion := 100
+
 func main() {
+
+	// init Sentry.io monitoring
+	// see docs https://docs.sentry.io/platforms/go/
+
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://<key>@sentry.io/<project>",
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(2 * time.Second)
+
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		fmt.Println("Error while getting hostname: ", err)
+		os.Exit(1)
+	}
+
+	sentry.ConfigureScope(func(scope *sentry.Scope) {
+		scope.SetContext("character", map[string]interface{}{
+			"productName":        "OpenMOS Service",
+			"buildVersion":         100,
+			"hostname": hostname,
+		})
+	})
+
+	sentry.CaptureMessage("OpenMOS initialized at ", hostname)
 
 	// check if arguments were provided when invoked
 
